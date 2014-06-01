2dded0cb789348534efb5647cfce8dda8ccebc24
Michael Woloszuk <mwoloszuk@gmail.com>
2014-05-31 22:18:13 -0400

    Update to extension

--- content_script.js
+++ content_script.js
@@ -1,4 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.tts.speak('Welcome to Blinderino');
-});
-  
+window.onload = function () {
+	var inc = 0
+	  , paragraphs = document.getElementsByTagName('p');
+	document.body.addEventListener('keypress',keyPress,false);
+	function keyPress(e) {
+		if (e.keyCode === 13) {
+			var current = paragraphs[inc];
+			if (current) {
+				console.log(replaceHTML(current.innerHTML));
+				inc++
+			}
+		}
+	}
+	function replaceHTML(str) {
+	  var re = /&(nbsp|amp|quot|lt|gt);/g;
+	  return str.replace(re,'').replace(/(<([^>]+)>)/ig,'');
+	}
+};
